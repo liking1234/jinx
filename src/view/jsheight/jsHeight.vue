e5f09bf1578f79ca14fbf09a759fb6070031d261
早安 <oncwnuG4Kq7mRsygqPbGxB4TItWc@git.weixin.qq.com>
2022-01-17 09:17:01 +0800

    颜色

--- src/view/jsheight/jsHeight.vue
+++ src/view/jsheight/jsHeight.vue
@@ -51,6 +51,9 @@
       通过命令查找某一特定端口，在命令窗口中输入命令中输入netstat -ano |findstr "端口号"，然后回车就可以看到这个端口被哪个应用占用。
       查看到对应的进程id之后，就可以通过id查找对应的进程名称，使用命令tasklist |findstr "进程id号"
       通过命令杀掉进程，或者是直接根据进程的名称杀掉所有的进程，，在命令框中输入如下命令taskkill /f /t /im "进程id或者进程名称"
+
+      git push -> error 10054
+      git config --global http.sslVerify "false"
     </code>
   </pre>
   </div>
